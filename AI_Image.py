82b32327c7af1e132e09426d8c66f351de354a42
Yoav-85 <yoavzamir@haderahigh.org.il>
2025-02-21 11:38:53 +0200

    Add files via upload

--- AI_Image.py
+++ AI_Image.py
@@ -6,12 +6,12 @@ load_dotenv()
 
 # Get the API key from the environment
 XAI_API_KEY = os.getenv('XAI_API_KEY')
-base_url = "https://api.x.ai/v1"
+base_url = "https://api.aimlapi.com/v1"
 
 if not XAI_API_KEY:
     raise ValueError("XAI_API_KEY environment variable is not set.")
 
-client = OpenAIWrapper(api_key=XAI_API_KEY, base_url=base_url, model_name="grok-beta")
+client = OpenAIWrapper(api_key=XAI_API_KEY, base_url=base_url, model_name="gpt-4o")
 
 
 def ask_bot(text):
